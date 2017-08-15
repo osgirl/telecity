08605d25e49ca290208dfc746b8dedce3ccf056e
vorou <voropaev.viktor@gmail.com>
2017-08-15 08:57:13 +0500

    init

---
+++ src/telecity/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot;
+
+namespace telecity
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			var bot = new TelegramBotClient(File.ReadAllText(@"C:\elbatelecitybot.key"));
+			bot.OnMessage += (o, args) => Console.WriteLine(args.Message.Text);
+			bot.StartReceiving();
+			Console.Out.WriteLine("Listening, press ENTER to quit...");
+			Console.ReadLine();
+		}
+	}
+}
